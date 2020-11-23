b172d3461642e6e51c6d6a4a1cb8fb4006bb3c38
RajathShanmugaiah <63197365+RajathShanmugaiah@users.noreply.github.com>
2020-11-23 17:12:11 +0100

    form html,css,js task

---
+++ CSS Exercise/Form Component/form.js
@@ -0,0 +1,5 @@
+function submitFunc() {
+  var name = document.getElementById("FormName").value;
+  var email = document.getElementById("FormEmail").value;
+  alert("name :"+name+"  "+"  email :"+email);
+}
